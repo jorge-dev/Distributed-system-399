eac38b2ab3c648cfa8ed6c1d0df2f204f1774492
jorgeavilaBL <javila@blacklinesafety.com>
2022-03-14 23:08:02 -0600

    setup env stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jorge-dev/Distributed-system-559
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- src/main.go
+++ src/main.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/jorge-dev/Distributed-system-559/src/client"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Errorf("Error loading .env file")
+		os.Exit(1)
+	}
+
+	// fmt.Printf("%s\n", os.Getenv("TEST_ENV"))
+	// os.Exit(0)
+
 	// get host and port from command line
 	args := os.Args[1:]
 	if len(args) != 2 {
@@ -21,7 +31,7 @@ func main() {
 	port := args[1]
 
 	// connect to the server
-	err := client.Connect(host, port)
+	err = client.Connect(host, port)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
