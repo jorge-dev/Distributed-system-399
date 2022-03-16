00ca0ca6d8eb71417c8dc439c87a865ac3abc849
jorge-dev <jo.luisa94@gmail.com>
2022-03-15 18:28:19 -0600

    added lgrus package

--- src/main.go
+++ src/main.go
@@ -7,18 +7,49 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/jorge-dev/Distributed-system-559/src/client"
+	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	err := godotenv.Load()
+func getLogLevel() log.Level {
+	switch os.Getenv("LOG_LEVEL") {
+	case "debug":
+		return log.DebugLevel
+	case "info":
+		return log.InfoLevel
+	case "warn":
+		return log.WarnLevel
+	case "error":
+		return log.ErrorLevel
+	case "fatal":
+		return log.FatalLevel
+	case "panic":
+		return log.PanicLevel
+	default:
+		return log.InfoLevel
+	}
+}
+
+func init() {
+	err := godotenv.Load("../.env")
 	if err != nil {
-		fmt.Errorf("Error loading .env file")
+		log.Fatal("Error loading .env file")
+		fmt.Println("Error loading .env file")
 		os.Exit(1)
 	}
 
-	// fmt.Printf("%s\n", os.Getenv("TEST_ENV"))
-	// os.Exit(0)
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
 
+	log.SetOutput(os.Stdout)
+	log.SetLevel(getLogLevel())
+	fmt.Println("Log level: ", log.GetLevel())
+
+	log.Info("Logging initialized")
+
+}
+
+func main() {
 	// get host and port from command line
 	args := os.Args[1:]
 	if len(args) != 2 {
@@ -31,9 +62,10 @@ func main() {
 	port := args[1]
 
 	// connect to the server
-	err = client.Connect(host, port)
+	err := client.Connect(host, port)
 	if err != nil {
 		fmt.Println("Error: ", err)
+		log.WithField("error", err).Error("Error connecting to server")
 		os.Exit(1)
 	}
 }
