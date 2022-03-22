a9019bc5bd6cbebd0fd39e365bce44333bbd33f0
jorgeavilaBL <javila@blacklinesafety.com>
2022-03-21 22:51:33 -0600

    organize and added more test registries

---
+++ registries/registryIter1/registry/Peer.java
@@ -0,0 +1,17 @@
+package registry;
+
+public class Peer {
+	String address;
+	int port;
+	String teamName;
+
+	Peer[] peersSent = null;
+
+	String key() {
+		return teamName;
+	}
+
+	public String toString() {
+		return key() + " " + address + ":" + port;
+	}
+}
